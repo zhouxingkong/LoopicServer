b9e7eca5bfb61ff14bdfd543f990519b3b54dd10
xingkong <zhouxingyu.zoe@bytedance.com>
2021-12-03 14:16:26 +0800

    简化程序

--- src/main/kotlin/xingkong/loopicserver/module/bean/TagedFile.kt
+++ src/main/kotlin/xingkong/loopicserver/module/bean/TagedFile.kt
@@ -3,15 +3,15 @@ package xingkong.loopicserver.module.bean
 import java.io.File
 import java.util.*
 
-class TagedFile(var file: File,val inputTag:List<String>) {
+class TagedFile(var file: File,val tags:List<String>) {
 //    var mp = 100.0     //匹配分，越小说明和目标标签越近
-    var tags = mutableListOf<String>()
+//    var tags = mutableListOf<String>()
     val name: String
         get() = file.name
     val absolutePath: String
         get() = file.absolutePath
 
-    init {
-        tags.addAll(inputTag)
-    }
+//    init {
+//        tags.addAll(inputTag)
+//    }
 }
